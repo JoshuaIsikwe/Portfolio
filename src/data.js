037888cd738ab4f6aff1503b4ec07fcd14dac677
JoshuaIsikwe <IsikweJoshua@gmail.com>
2022-10-05 09:39:10 +0200

    New data

--- src/data.js
+++ src/data.js
@@ -7,8 +7,8 @@ export const products =[
     },
     {
         id: 2,
-        img: "https://i.pinimg.com/474x/42/f9/ce/42f9cef4cfe54e1cfdf85e3025f44803.jpg",
-        link: "https://clemmeridian.com/",
+        img: "https://i.pinimg.com/564x/b7/4f/4a/b74f4a0a368dab4773c509542ca130dd.jpg",
+        link: "https://clemmeridian.org/",
     },
     {
         id: 3,
